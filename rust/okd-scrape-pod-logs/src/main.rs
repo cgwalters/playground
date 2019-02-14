5d16d6f6f0c59c4126ceb7f45f0ba55788f6ffeb
Colin Walters <walters@verbum.org>
2019-02-14 14:54:12 +0000

    wip

--- rust/okd-scrape-pod-logs/src/main.rs
+++ rust/okd-scrape-pod-logs/src/main.rs
@@ -11,9 +11,13 @@ lazy_static! {
     static ref PR_RE: Regex = Regex::new(r"/build/([^/]+)/(.+)").unwrap();
 }
 
-fn gcs_get(client: &Client, bucket: &str, object: &str) -> Fallible<String> {
-
-    reqwest
+fn gcs_get(client: &reqwest::Client, bucket: &str, object: &str) -> Fallible<String> {
+    let baseurl = Url::parse("https://www.googleapis.com/").unwrap();
+    let path = format!("storage/v1/b/{}/{}", bucket, object);
+    let url = baseurl.join(&path)?;
+    (|| {
+        client.get(url.as_str()).send()?.text()
+    })().map_err(|e|failure::err_msg(e.to_string()))
 }
 
 fn main() -> Fallible<()> {
@@ -25,7 +29,8 @@ fn main() -> Fallible<()> {
     let url = Url::parse(urlstr)?;
     let path = url.path();
     let caps = PR_RE.captures(path).ok_or_else(|| failure::err_msg("URL does not match OpenShift PR regexp"))?;
-    let buf : String = reqwest::get(urlstr)?.text()?;
+    let client = reqwest::Client::new();
+    let buf = gcs_get(&client, caps.get(1).unwrap().as_str(), caps.get(2).unwrap().as_str())?;
     println!("{}", buf);
     Ok(())
 }
