c151a8545e58acd6d9963156f99d2d42150e4ae5
Colin Walters <walters@verbum.org>
2019-09-08 19:14:39 +0000

    More commandspec hacking

--- scripts/virt-install-coreos.rs
+++ scripts/virt-install-coreos.rs
@@ -8,9 +8,11 @@
 //! [dependencies]
 //! quicli = "0.4"
 //! structopt = "0.2"
+//! commandspec = "0.12.2"
 //! ```
 use quicli::prelude::*;
 use structopt::StructOpt;
+use commandspec::{command,execute}; 
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -29,7 +31,11 @@ fn main() -> CliResult {
     let args = Opts::from_args();
     args.verbosity.setup_env_logger("head")?;
 
-    dbg!(&args.disk);
-    dbg!(&args.uri);
+    execute!(
+        r"
+            ls -al {disk}
+        ",
+        disk=args.disk.as_str(),
+    )?;
     Ok(())
 }
