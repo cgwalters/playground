d72d980b66cc2cb84e658140e5a7406691457aad
Colin Walters <walters@verbum.org>
2020-04-02 18:59:49 +0000

    rhcos-nvme: Fixes to better simulate and handle AWS setup

--- ignition/rhcos-two-nvme.sh
+++ ignition/rhcos-two-nvme.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-exec kola qemuexec --auto-cpus --ignition-direct \
-     --qemu-size 300G --qemu-image /srv/walters/pubannex/machine-images/rhcos/rhcos-44.81.202004020530-0-qemu.x86_64.qcow2 \
-     --qemu-nvme -i rhcos-raid-containers.json -- \
-     -device nvme,drive=disk2,serial=2 -drive if=none,id=disk2,file=/tmp/disk,auto-read-only=off
+set -euo pipefail
+qemu-img create -f qcow2 /tmp/disk1 300G
+qemu-img create -f qcow2 /tmp/disk2 300G
+exec kola qemuexec --auto-cpus \
+     --qemu-image /srv/walters/pubannex/machine-images/rhcos/rhcos-4.3.8-x86_64-qemu.x86_64.qcow2 \
+     --qemu-nvme --ignition-direct -i "$1" -- \
+     -device nvme,drive=i1,serial=i1 -drive if=none,id=i1,file=/tmp/disk1,auto-read-only=off \
+     -device nvme,drive=i2,serial=i2 -drive if=none,id=i2,file=/tmp/disk2,auto-read-only=off
