0d6868306e716e7051949ab7a24a5ade1daa1552
Colin Walters <walters@verbum.org>
2023-04-04 18:17:41 -0400

    Add ipxe for equinix w/ssh

--- rust/failpoint-test/src/main.rs
+++ rust/failpoint-test/src/main.rs
@@ -1,7 +1,9 @@
+use std::os::unix::fs::MetadataExt;
+
 fn main() -> anyhow::Result<()> {
-    fail::fail_point!("main", true, |msg| {
-        let msg = msg.as_deref().unwrap_or("synthetic error");
-        Err(anyhow::anyhow!("{msg}"))
-    });
+    let name = std::env::args().nth(1).unwrap();
+    let meta = std::fs::metadata(name)?;
+    let dev = meta.dev();
+    println!("{dev:?}");
     Ok(())
 }
